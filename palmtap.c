95418a1dff05fb0b7f0ad107ec23177910fe6c1c
mikewax <spillikinaerospace@yahoo.com>
2016-07-07 17:44:24 -0700

    Add files via upload

--- palmtap.c
+++ palmtap.c
@@ -1,5 +1,5 @@
 
-unsigned int input;
+unsigned int input, preinput;
 int i, j;
 
 void setup() { 
@@ -7,31 +7,17 @@ for ( i = 16; i < 20; i++) pinMode( i, OUTPUT); // strobe
 for ( j = 20; j < 24; j++) pinMode( j,  INPUT_PULLUP); // probe
 } 
 
-void poll(unsigned int input) { 
+unsigned int poll() { 
 input = 0;
 for ( i = 16; i < 20; i++) {
 digitalWrite( i, LOW ); delay(1);
 for ( j = 20; j < 24; j++) { input << 1; if ( !digitalRead(j) ) input++;  }
 digitalWrite( i, HIGH ); }
-return input; }
-
-
-
-{ // D7 pin is high due to pullup resistor 
-digitalWrite(PIN_D6, HIGH); // LED off 
-delay(400); 
+return input; 
 }
- else 
-{ // D7 pin is low due to pushbutton pressed 
-digitalWrite(PIN_D6, LOW); // LED on 
-delay(80); // Fast blink 
-digitalWrite(PIN_D6, HIGH); // LED off 
-delay(80); 
-}
- } 
 
+void processInput() {
 
-
-
-
+preinput = input;
+input = poll();
 
